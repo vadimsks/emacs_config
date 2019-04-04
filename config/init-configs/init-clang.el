c7927275f573d6f6ae881d7fb358eadaf6113364
user <you@example.com>
2019-04-04 23:56:06 +0300

    Fixed clang path

--- config/init-configs/init-clang.el
+++ config/init-configs/init-clang.el
@@ -1,13 +1,14 @@
 
-(setq my-clang-dir "/home/developer/work/clang/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-16.04/")
+(setq my-clang-dir "~/work/clang/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-16.04")
 
-(let* ((el-file (concat my-clang-dir "share/clang/clang-format.el") )
-       (exe-file (concat my-clang-dir "/bin/clang-format") ) )
+(let* ((dir (expand-file-name my-clang-dir))
+       (el-file (concat dir "/share/clang/clang-format.el") )
+       (exe-file (concat dir "/bin/clang-format") ) )
   (if (and (file-directory-p my-clang-dir) (file-exists-p el-file))
       (progn
         (load el-file)
         (require 'clang-format)
         (setq clang-format-executable exe-file)
         (global-set-key [C-tab] 'clang-format-region) )
-    (message "Cannot find clan in %s" my-clang-dir) ) )
+    (message "clang not found in %s" dir) ) )
 
