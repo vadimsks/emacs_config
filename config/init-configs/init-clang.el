9a2631fdfc05d83389ec953dda17169f10fca283
Name <developer@kili-vm>
2019-04-05 19:59:42 +0300

    Updated clang. Added eglot

--- config/init-configs/init-clang.el
+++ config/init-configs/init-clang.el
@@ -1,3 +1,6 @@
+(use-package eglot
+  :ensure t)
+(require 'eglot)
 
 (setq my-clang-dir "/home/developer/work/clang/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-16.04/")
 
@@ -8,6 +11,12 @@
         (load el-file)
         (require 'clang-format)
         (setq clang-format-executable exe-file)
-        (global-set-key [C-tab] 'clang-format-region) )
+        (global-set-key [M-q] 'clang-format-region)
+        (define-key c-mode-map (kbd "M-q") 'clang-format-region)
+        (define-key c++-mode-map (kbd "M-q") 'clang-format-region)
+        (add-to-list 'eglot-server-programs '((c++-mode c-mode) "/home/developer/work/clang/clang+llvm-8.0.0-x86_64-linux-gnu-ubuntu-16.04/bin/clangd")))
     (message "Cannot find clan in %s" my-clang-dir) ) )
 
+;; eglot - starts clangd, requires <root>/compile_commands.json
+;; xref-find-definitions
+;; company-capf
