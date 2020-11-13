3119b4a9a1d1496e16164777cee78ac0aede99c8
user <you@example.com>
2020-11-13 20:47:05 +0200

    Fixed eol ^M

--- config/init-main.el
+++ config/init-main.el
@@ -1,5 +1,5 @@
 ;; .emacs contents
-;; (setq custom-file "/home/user/work/emacs/emacs_config/config/init-main.el")
+;; (setq custom-file "/home/user/work/emacs/emacs_config/config/init-main.el")
 ;; (load custom-file 'noerror)
 
 
@@ -300,7 +300,7 @@
 
 ;; Elisp: Get Script Name at Run Time, Call by Relative Path
 ;; http://ergoemacs.org/emacs/elisp_relative_path.html
-;;(setq my-init-configs-dir "~/work/emacs/emacs_config/config/init-configs/")
+;;(setq my-init-configs-dir "~/work/emacs/emacs_config/config/init-configs/")
 
 (setq my-init-configs-dir
       (concat
