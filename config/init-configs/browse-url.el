8ddc313bec24eea28d5a0174902955157d3218c4
user <user@example.com>
2025-03-28 00:07:32 +0200

    google-chrome browser on MacOS

--- config/init-configs/browse-url.el
+++ config/init-configs/browse-url.el
@@ -5,10 +5,13 @@
 
 
 ;; Windows
-(if (eq system-type 'windows-nt)
-    (setq  browse-url-generic-program "c:/Program Files/Google/Chrome/Application/chrome.exe")
-  (setq browse-url-generic-program "/usr/bin/google-chrome")
-  )
+(setq browse-url-generic-program
+      (cond
+       ((eq system-type 'windows-nt) "c:/Program Files/Google/Chrome/Application/chrome.exe")
+       ((eq system-type 'darwin) "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome")
+       (t "/usr/bin/google-chrome")
+       )
+      )
 
 ;; use conkeror as a default web browser
 ;; (setq browse-url-browser-function 'browse-url-generic
